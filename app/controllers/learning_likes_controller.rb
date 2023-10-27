31e0d824c79f2304424a9a57731d770c0433a71b
numan2023 <fullmetal0212@gmail.com>
2023-10-27 21:08:48 +0900

    commit version1

--- app/controllers/learning_likes_controller.rb
+++ app/controllers/learning_likes_controller.rb
@@ -1,23 +1,15 @@
 class LearningLikesController < ApplicationController
-  before_action :set_learning
 
   def create
     like = current_user.learning_likes.build(learning_id: params[:learning_id])
     like.save
-    respond_to do |format|
-      format.js
-    end
+    redirect_to action: "/learnings"
   end
 
   def destroy
     like = LearningLike.find_by(learning_id: params[:learning_id], user_id: current_user.id)
     like.destroy
-    respond_to do |format|
-      format.js
-    end
+    redirect_to action: "/learnings"
   end
 
-  def set_learning
-    @learning = Learning.find(params[:learning_id])
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,21 @@
 Rails.application.routes.draw do
   devise_for :users
   root "posts#index"
+  
   resources :learnings, only: [:index, :new, :create, :edit, :update] do
     collection do
       get 'search'
     end
     resource :learning_likes, only: [:create, :destroy]
   end
+
   resources :jobs, only: [:index, :new, :create, :edit, :update] do
     collection do
       get 'search'
     end
     resource :job_likes, only: [:create, :destroy]
   end
+
   resources :others, only: [:index, :new, :create]
   resources :users, only: [:index, :edit, :update]
 end
